8c5c523b13f82a3c99eb7aaa5c486145eb6ffb61
Iury Bernardes <bernardes.iury@gmail.com>
2024-08-09 23:00:29 -0300

    First push

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
+import cors from "cors";
 import express, { type Request, type Response } from "express";
 import users from "./routes/users";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+//Middleware
+app.use(cors());
 
 app.get("/health", (req: Request, res: Response) => {
 	return res.json({ Message: "Running ok" });
